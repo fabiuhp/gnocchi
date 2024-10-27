81824b436a616e885a48ffe429bf6ae01001153c
Fabio Pereira <fabiuhp@msn.com>
2024-10-27 20:56:48 -0300

    inicio

---
+++ src/main/java/br/com/fabiopereira/gnocchi/GnocchiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabiopereira.gnocchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnocchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnocchiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabiopereira/gnocchi/GnocchiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiopereira.gnocchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnocchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
